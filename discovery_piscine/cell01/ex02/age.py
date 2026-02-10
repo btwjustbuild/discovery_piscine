36f1497b1bb277ef624f03e05118f6c150554c55
btwjustbuild <btwjustbuild@gmail.com>
2026-02-10 21:30:06 +0700

    Delete discovery_piscine directory

--- discovery_piscine/cell01/ex02/age.py
+++
@@ -1,4 +0,0 @@
-Build_age = 20
-x = 42
-my_age = Build_age + x
-print(my_age)
--- discovery_piscine/cell02/ex02/password.py
+++
@@ -1,8 +0,0 @@
-password = "Build"
-
-user_input = input("")
-
-if user_input == password:
-    print("ACCESS GRANTED")
-else:
-    print("ACCESS DENIED")
