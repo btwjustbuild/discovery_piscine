61400c2690e076be9587d79881f815e242b7f0c2
btwjustbuild <btwjustbuild@gmail.com>
2026-02-10 20:53:53 +0700

    Add files via upload

---
+++ discovery_piscine/cell01/ex00/name.py
@@ -0,0 +1,4 @@
+first_name = "Natthapong"
+last_name = "Suphawirojlert"
+
+print(first_name, last_name)
---
+++ discovery_piscine/cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Natthapong"
+last_name = "Suphawirojlert"
+whole_name = last_name + " " + first_name
+print(whole_name)
+
